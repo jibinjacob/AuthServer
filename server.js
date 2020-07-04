2f87bd285413eec6aaac4f05b10dfc7d4a5613e7
Jibin Jacob <jibin@mail.com>
2020-07-04 22:18:48 +0200

    bcryptjs added to hash user password

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
-const shortid = require('shortid')
+const bcrypt = require('bcryptjs')
 
 const adapter = new FileSync('db.json')
 const db = low(adapter)
@@ -20,17 +20,20 @@ app.get('/',(req,res) => res.send("Hello world"))
  * Register new user
  */
 app.post('/users',(req,res) => {
-    id = db.get('users')
-      .size()
-      .value()
+    
+  var salt = bcrypt.genSaltSync(10);
+  var hash = bcrypt.hashSync(req.body.password, salt);
+  id = db.get('users')
+    .size()
+    .value()
 
-    db.get('users')
-      .push({id : id+1,
-             username: req.body.username, 
-             email: req.body.email,
-             password: req.body.password } )
-      .write()
-    res.status(200).send("Hello "+req.body.username)
+  db.get('users')
+    .push({id : id+1,
+            username: req.body.username, 
+            email: req.body.email,
+            password: hash  } )
+    .write()
+  res.status(200).send("New user registered "+req.body.username)
 })
 
 app.listen(PORT, () => console.log(`Server started on port:${PORT}`)) 
